cef5c1f0cd79d7ab50a80c138e92c93d6a025b88
ImNotTheBoi <174925899+ImNotTheBoi@users.noreply.github.com>
2025-04-02 19:18:12 +0800

    Navigation Done

--- src/homepage.js
+++ src/homepage.js
@@ -1,10 +1,11 @@
 import burger from "./imgs/burger.png";
 
 export default function loadHomePage() {
+    const content = document.querySelector(".content")
     const container = document.createElement("div")
     const image = document.createElement("img")
     container.classList.add("main-burger")
     image.src = burger
     container.appendChild(image)
-    document.body.appendChild(container)
+    content.appendChild(container)
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import "./styles.css";
 import loadHomePage from "./homepage.js"
+import loadMenuPage from "./menupage.js"
+import loadAboutPage from "./aboutpage.js"
 
-loadHomePage()
+//* Buttons are given functions based on table index
+const content = document.querySelector(".content")
+const aboutButton = document.querySelector(".about")
+const homeButton = document.querySelector(".home")
+const menuButton = document.querySelector(".menu")
+const pageButtons = [aboutButton, homeButton, menuButton]
+const loadFunctions = [loadAboutPage, loadHomePage, loadMenuPage]
 
 function loadButtons(){
-    
+    for (let i = 0; i < pageButtons.length; i++) {
+        pageButtons[i].addEventListener('click', () => {
+            content.innerHTML = ""
+            loadFunctions[i]()
+            highlightButton(pageButtons[i])
+        })
+        console.log(i)
+    }
+}
+
+function highlightButton(button){
+    pageButtons.forEach((element) => {
+        element.classList.remove("highlight")
+    })
+    button.classList.toggle("highlight")
 }
 
 function deleteContent() {
     
 }
+
+loadButtons()
